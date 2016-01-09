271448220463b2c50f1622831885ff857bbcded9
wangqiccc <291315268@qq.com>
2016-01-09 15:07:53 +0800

    修改首页js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(".left>ul>li").click(function(){
+    $(this).children("ul").finish()
 	$(this).children("ul").slideToggle()
 	$(this).siblings().children("ul").slideUp()
 })
@@ -6,4 +7,6 @@ $(".left>ul>li>ul a").click(function(e){
 	e.stopPropagation()	
 	 $(".left>ul a").removeClass("active")
 	 $(this).addClass("active")
+	 $(this).parent().find("ul").stop();
+	 $(this).parent().find("ul").slideToggle()
 })
